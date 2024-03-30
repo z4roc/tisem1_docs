8e6904de265e2f0da4864adf7bb5aa5fc6510e53
Arthur Aktamirov <arthuraktamirov@gmail.com>
2024-03-30 16:57:23 +0100

    create page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc67831ebab691dc"; }
+/******/ 		__webpack_require__.h = function() { return "97730b2be727095b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
