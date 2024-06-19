3551a32be085199a3c9597e615836d92c648c44c
Arthur Aktamirov <arthuraktamirov@gmail.com>
2024-06-19 04:08:00 +0200

    update structure

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  i18n: {
+    locales: ["de"],
+    defaultLocale: "de",
+  },
+});
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,30 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>Bachelorstudium Informatik HS Albsig</span>,
   project: {
     link: "https://github.com/z4roc",
+  },
+  i18n: [{ locale: "de", text: "Deutsch" }],
+  docsRepositoryBase: "https://github.com/z4roc/tisem1_docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Bachelorstudium Informatik HS Albsig",
+    };
+  },
+  footer: {
+    text: "Bachelorstudium Informatik HS Albsig",
+  },
+  toc: {
+    title: "Auf dieser Seite",
+    backToTop: true,
+  },
+  editLink: {
+    text: "Diese Seite bearbeiten",
+  },
+  feedback: {
+    content: "Fragen? Gib mir Feedback!",
+  },
+  search: {
+    placeholder: "Dokumentation durchsuchen",
   },
   // ... other theme options
 };
